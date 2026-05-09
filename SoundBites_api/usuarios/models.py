fcf41aa795eda75458c37cae225a33cdee845fa3
valeriabet <valeriabetancourt013@gmail.com>
2026-05-08 20:30:41 -0500

    Coneción con base de datos exitosa, migraciones realizadas y creacion del entorno e instalacion de dependencias

---
+++ SoundBites_api/usuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+# Create your models here.
